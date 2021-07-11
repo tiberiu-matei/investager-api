7019adc2ea6c796cd550862e1b1261aa83cb578d
Tibi <matei.tiberiu02@gmail.com>
2021-07-11 22:26:58 +0100

    Add log endpoint (#17)

---
+++ Investager.Api/Controllers/LogController.cs
@@ -0,0 +1,44 @@
+﻿using Investager.Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog.Events;
+
+namespace Investager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Log([FromBody] LogRequest request)
+        {
+            switch (request.Level)
+            {
+                case LogEventLevel.Error:
+                    _logger.LogInformation(request.Message);
+                    break;
+                case LogEventLevel.Warning:
+                    _logger.LogWarning(request.Message);
+                    break;
+                case LogEventLevel.Debug:
+                    _logger.LogDebug(request.Message);
+                    break;
+                case LogEventLevel.Information:
+                case LogEventLevel.Verbose:
+                case LogEventLevel.Fatal:
+                default:
+                    _logger.LogInformation(request.Message);
+                    break;
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ Investager.Infrastructure/Models/LogRequest.cs
@@ -0,0 +1,11 @@
+﻿using Serilog.Events;
+
+namespace Investager.Infrastructure.Models
+{
+    public class LogRequest
+    {
+        public LogEventLevel Level { get; set; }
+
+        public string Message { get; set; }
+    }
+}
